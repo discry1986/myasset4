e62a85ec9595dda02869ab496704e4163a150af4
discry1986 <565198745@qq.com>
2017-12-12 22:12:36 +0800

    删除脚本sql

--- src/main/resources/sql/1_projectType_init.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `myasset4`.`sys_ptype`(`createdate`,`name`,`number`)VALUES('2017-11-02 22:10:56','固定工资','A.01');
-INSERT INTO `myasset4`.`sys_ptype`(`createdate`,`name`,`number`)VALUES('2017-11-02 22:10:56','投资理财','B.01');
-INSERT INTO `myasset4`.`sys_ptype`(`createdate`,`name`,`number`)VALUES('2017-11-02 22:10:56','第三产业','C.01');
-INSERT INTO `myasset4`.`sys_ptype`(`createdate`,`name`,`number`)VALUES('2017-11-02 22:10:56','租金收入','D.01');
